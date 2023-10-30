df6c3298f98fc47c7ce9ceff38737db682cd2ed2
HarukaSan115 <Jorgemastter27@gmail.com>
2023-10-30 12:44:58 -0500

    Interfaz para cargar efectos

--- app/src/main/java/com/example/pedalp1/MainActivity.java
+++ app/src/main/java/com/example/pedalp1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.pedalp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner Lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Lista = (Spinner) findViewById(R.id.spnPreinstalados);
+        String [] Efectos = {"Clean", "Distortion", "Boost"};
+        ArrayAdapter <String> AdaptadorEfecto = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Efectos);
+        Lista.setAdapter(AdaptadorEfecto);
+
     }
 }
