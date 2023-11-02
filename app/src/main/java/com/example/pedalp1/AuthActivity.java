35fbba0999d144d4c985bb47f8295f2af1197ce3
HarukaSan115 <Jorgemastter27@gmail.com>
2023-11-02 18:11:57 -0500

    Se sustituyen las dependencias MQTT del proyecto por las dependencias FTP, se crea la clase para enviar el archivo y la configuracion del servicio FTP y se crea el evento al pulsar el boton "cargar efecto"

--- app/src/main/java/com/example/pedalp1/AuthActivity.java
+++ app/src/main/java/com/example/pedalp1/AuthActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.analytics.FirebaseAnalytics;
